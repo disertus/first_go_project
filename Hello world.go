27a625597578d3c8102089f724e40d53289e35c8
Roman Zahorskyi <zagorsky.roman@gmail.com>
2020-10-21 22:01:25 +0300

    Added more functions and basic syntax

--- Hello world.go
+++ Hello world.go
@@ -2,23 +2,43 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"math"
+	"time"
 )
 
 //declaring functions outside of the main function
-func add(x int, y int) int {  //explainer: types are declared after the variable. output type outside arguments
+func add(x int, y int) int { //explainer: types are declared after the variable. output type outside arguments
 	return x + y
 }
 
-func substract(x, y int) int{ //if both arguments are of same type, one can put the type at the end without repeating
+func subtract(x, y int) int { //if both arguments are of same type, one can put the type at the end without repeating
 	return x - y
 }
 
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func split(sum float32) (x, y float32) { //if the result is a float, arguments should be floats too
+	x = sum * 4 / 9
+	y = sum - x
+	return x, y
+}
+
+var c, python, java bool //declares a list of variables
+
 func main() {
-	fmt.Println("Hello, world")
-	fmt.Println("The time is", time.Now()) //string concatenation example
-	fmt.Printf("Now I have %g problems\n", math.Sqrt(7))  //formatted string example
-	fmt.Println(add(150, 450)) //example of outer function execution with two positional arguments
-	fmt.Println(substract(1000, 933))
+	fmt.Println("Hello, world")                          //Function accepts only double quotes "..." for strings
+	fmt.Println("The time is", time.Now())               //string concatenation example
+	fmt.Printf("Now I have %g problems\n", math.Sqrt(7)) //formatted string example
+	fmt.Println(add(150, 450))                           //example of outer function execution with two positional arguments
+	fmt.Println(subtract(1000, 933))
+
+	a, b := swap("hello", "world") //variables contain the respective values returned by swap()
+	fmt.Println(a, b)
+
+	fmt.Println(split(19))
+
+	var i int
+	fmt.Println(i, c, python, java) // why it prints false three times?
 }
